8122ad0ff015a3c7ed9f2021c8359303880df4e9
D.Danilova <ddanilova@abdigital.uz>
2024-10-24 19:37:11 +0500

    game-ready

--- newApp.js
+++ newApp.js
@@ -2,64 +2,26 @@ new Vue({
     el: '#vue-app',
     data() {
         return {
-            name: 'DD',
-            something: 'this is sub-header special note',
-            repositoryName: 'My Repository',
-            repositoryURL: 'https://github.com/StatusQvo/vuejsStart',
-            isActive: false,
-            isBold: false,
-            isPushedBtn1: false,
-            isBayMaxXs: true,
-            isBayMaxXl: false,
-            less5Events: "Events",
-            myAge: 36,
-            X: 0,
-            Y: 0,
-        }
-    },
-    computed: {
-        classObject() {
-            return {
-                superClass: this.isActive,
-                boldClass: this.isBold
-            }
-        },
-        BaymaxSize() {
-            return {
-                Baymax_xs: this.isBayMaxXs,
-                Baymax_xl: this.isBayMaxXl
-            }
+            health: 100,
+            ended: false,
+            isRestart: false,
+            hitStrength: 10,
         }
     },
     methods: {
-        toggleClasses() {
-            this.isActive = !this.isActive;
-            this.isBold = !this.isBold;
-            this.isPushedBtn1 = !this.isPushedBtn1;
-            this.isBayMaxXs = !this.isBayMaxXs;
-            this.isBayMaxXl = !this.isBayMaxXl;
+        punch: function(hitPoint) {
+            if (this.health - hitPoint > 0) {
+                this.health -= hitPoint;
+            } else {
+                this.health = 0;
+                this.ended = !this.ended;
+                this.isRestart = !this.isRestart;
+            }
         },
-        greeting: function(daytime) {
-            return `Good ${daytime} ${this.name} and Hello world`;
-        },
-        URLTag() {
-            return `<a href="${this.repositoryURL}">${this.repositoryName} by Tag</a>`;
-        },
-        test() {
-            return '11111';
-        },
-        myAgeAdd(Year) {
-            this.myAge += Year;
-        },
-        myAgeRm(Year) {
-            this.myAge -= Year;
-        },
-        updateXY(event) {
-            this.X = event.offsetX;
-            this.Y = event.offsetY;
-        },
-        click: function() {
-            alert('You clicked me!');
+        restart: function() {
+            this.health = 100;
+            this.ended = false;
+            this.isRestart = false;
         }
     }
 });
