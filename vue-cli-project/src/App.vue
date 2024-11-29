313e0f1a0ca592ba267c1f03ca9b23b8c45553f8
D.Danilova <ddanilova@abdigital.uz>
2024-11-29 18:26:23 +0500

    lesson 22

--- vue-cli-project/src/App.vue
+++ vue-cli-project/src/App.vue
@@ -1,34 +1,40 @@
 <template>
-<div>
-    <div>
-      <h1>{{ title }}</h1>
-      <p>{{ greeting() }}</p>
-      
-    </div><mnn></mnn>
-  </div>
+  <div>
+    <app-header></app-header>
+    <app-root v-bind:app-users="Users"></app-root>
+    <app-footer></app-footer>
+  </div> 
+
 </template>
 
 <script>
-import nnm from './ddd.vue';
+import Header from "./components/header.vue";
+import Footer from "./components/footer.vue";
+import Root from "./components/mainRoot.vue"
 
-export default {
-    components: {
-    'mnn': nnm
-    },
-  data () {
+export default{
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-root': Root,
+  },
+  data() {
     return {
-      title: "My first VUE file, wohoo!!!"
-    }
-  },
-  methods: {
-    greeting: function(){
-      return "Hey, WOOOOOW!"
-    }
-  },
-
+           'Users':[
+                    {userName: 'Den', occupation: 'Front-end Developer', show: false},
+                    {userName: 'Shon', occupation: 'Back-end Developer', show: false},
+                    {userName: 'Dina', occupation: 'Assistant', show: false},
+                    {userName: 'Clark', occupation: 'PM', show: false},
+                    {userName: 'Sten', occupation: 'Designer', show: false},
+                ],
+          }
+  }
 }
 </script>
 
-<style scoped>
-h1{color: red}
+<style>
+  body{
+      margin: 0;
+      font-family: 'Nunito SemiBold';
+  }
 </style>
---
+++ vue-cli-project/src/components/footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer>
+        <p>{{ copyright }}</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            'copyright': "Copyright 29.11.2024 VUE Lessons"
+        }
+    }
+}
+
+</script>
+
+
+<style scoped>
+footer{
+    background: #222;
+    padding: 6px;
+}
+p{
+        color: lightgreen;
+        text-align: center;
+}
+</style>
