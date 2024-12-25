9ff613bbcc2041ad78970941b9767d3f4ff65c22
D.Danilova <ddanilova@abdigital.uz>
2024-12-25 18:18:40 +0500

    lesson 30 - continue

--- vue-cli-project/src/App.vue
+++ vue-cli-project/src/App.vue
@@ -1,41 +1,28 @@
 <template>
   <div>
-    <app-header v-bind:title="title"></app-header>
-    <app-root v-bind:appUsers="Users"></app-root>
-    <hr>
-    <app-root v-bind:appUsers="Users"></app-root>
-    <app-footer v-bind:title="title"></app-footer>
+<add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import Header from './components/header.vue';
-import Footer from './components/footer.vue';
-import Root from './components/root.vue';
+//imports
+import addBlog from "./components/addBlog.vue"
+
 export default {
-  components: {
-    'appHeader': Header,
-    'appFooter': Footer,
-    'appRoot': Root,
-  },
-  data() {
-    return {   
-      'Users': [
-        { userName: 'Den', occupation: 'Front-end Developer', show: false },
-        { userName: 'Shon', occupation: 'Back-end Developer', show: false },
-        { userName: 'Dina', occupation: 'Assistant', show: false },
-        { userName: 'Clark', occupation: 'PM', show: false },
-        { userName: 'Sten', occupation: 'Designer', show: false },
-      ],
-      title: 'Vue Project'
-    };
-  },
+    components: {
+        'add-blog': addBlog,
+    },
+    data () {
+        return {
+        }
+    },
+
 }
 </script>
 
 <style>
-  body {
+body{
     margin: 0;
     font-family: 'Nunito SemiBold';
-  }
+}
 </style>
